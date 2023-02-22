dcd7f1adc5ddd48db3c3e82de26fe1661a55fa2b
Madi <abaymadi1@gmail.com>
2023-02-22 19:23:05 +0600

    practice version

--- items/views.py
+++ items/views.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from django.views import View
 
 from django.shortcuts import get_list_or_404
 
@@ -38,19 +37,6 @@ class DetailItem(DetailView):
         return context
 
 
-# class ProductLandingPageView(TemplateView):
-#     template_name = 'landing.html'
-
-#     def get_context_data(self, **kwargs):
-#         item = models.Item.objects.get(id=1)
-#         context = super(ProductLandingPageView, self).get_context_data(**kwargs)
-#         context.update({
-#             'item': item,
-#             'STRIPE_PUBLIC_KEY': settings.STRIPE_PUBLIC_KEY
-#         })
-#         return context
-
-
 @csrf_exempt
 def create_checkout_session(request, item_id):
     if request.method == 'GET':
@@ -60,8 +46,8 @@ def create_checkout_session(request, item_id):
         stripe.api_key = settings.STRIPE_SECRET_KEY
         try:
             checkout_session = stripe.checkout.Session.create(
-                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
-                cancel_url=domain_url + 'cancelled/',
+                success_url=domain_url + '/success/',
+                cancel_url=domain_url + '/cancel/',
                 payment_method_types=['card'],
                 mode='payment',
                 line_items=[
@@ -80,38 +66,3 @@ def create_checkout_session(request, item_id):
             return JsonResponse({'sessionId': checkout_session['id']})
         except Exception as e:
             return JsonResponse({'error': str(e)})
-
-
-
-
-
-# class CreateCheckoutSessionView(View):
-#     def get(self, request, *args, **kwargs):
-#         if request.method == 'GET':
-#             item_id = self.kwargs['pk']
-#             item = models.Item.objects.get(id=item_id)
-#             print(item)
-#             stripe.api_key = settings.STRIPE_SECRET_KEY
-#             YOUR_DOMAIN = "http://127.0.0.1:8000/"
-#             checkout_session = stripe.checkout.Session.create(
-#                 payment_method_types=['card'],
-#                 line_items=[
-#                     {
-#                         'price_data': {
-#                             'currency': 'usd',
-#                             'unit_amount': item.price,
-#                             'item_data': {
-#                                 'name': item.name,
-#                             },
-#                         },
-#                         'quantity': 1,
-#                     },
-#                 ],
-#                 mode='payment',
-#                 success_url=YOUR_DOMAIN + '/success/',
-#                 cancel_url=YOUR_DOMAIN + '/cancel/',
-#             )
-
-#             return JsonResponse({
-#                 'id': checkout_session['id']
-#             })
--- src/urls.py
+++ src/urls.py
@@ -1,18 +1,3 @@
-"""src URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
@@ -21,9 +6,9 @@ from items import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('items/', views.ItemsView.as_view(), name='items'),
+    path('item/', views.ItemsView.as_view(), name='items'),
     path('success/', views.SuccessView.as_view(), name='success'),
     path('cancel/', views.CancelView.as_view(), name='cancel'),
-    path('items/<int:pk>/', views.DetailItem.as_view(), name='view_item'),
+    path('item/<int:pk>/', views.DetailItem.as_view(), name='view_item'),
     path('buy/<int:item_id>/', views.create_checkout_session, name='buy'),
 ]
